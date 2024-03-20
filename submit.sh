be51b964018283e452c2e6bccbe43035210775d5
Riya Verma <riyasinghverma@gmail.com>
2024-03-20 09:39:02 -0700

    Java parameters added

--- submit.sh
+++ submit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export REPO_PATH=/project/mlmccain_913/riya/calcium_analysis/snakemake
 os.environ['OPENBLAS_NUM_THREADS'] = '1' #cluter user openbladnumthreads has to be 1
+export JAVA_OPTS="-Xmx32g -XX:ParallelGCThreads=16" #java parameters for bfconvert
 
 if [ ! -e “slurm_logs” ]
 then
